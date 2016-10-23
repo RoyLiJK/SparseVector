dd8c6c701beca68f6410390113d7aa18a9a01450
ming <ming@ming-PC>
2016-10-22 20:06:40 -0400

    test

--- src/Project1.java
+++ src/Project1.java
@@ -8,22 +8,17 @@ public class Project1 {
 		a.add(3.0);
 		a.add(4.0);
 		a.add(5.0);
-		a.add(8.0);
-		a.add(6.0);
-		a.add(6.0);
+	
 		
 		for (int i =0; i<a.size();i++){
 			System.out.println(a.get(i));
 		}
-		int k = a.size();
-		for (int i =0; i<k;i++){
-			a.remove(0);
-			for (int j =0; j<a.size();j++){
-				System.out.println(a.get(j));
-			}
-
-		}
-//		a.remove(a.size()-3);
+		System.out.println("-----------------------");
+//		int k = a.size();
+//		for (int i =0; i<k;i++){
+//			a.remove(a.size()-1);
+//		}
+		a.remove(0);
 		for (int i =0; i<a.size();i++){
 			System.out.println(a.get(i));
 		}
