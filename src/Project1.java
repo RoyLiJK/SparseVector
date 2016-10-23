61ea8577178c75d8667c808bfcad0b957f3b9090
ming <ming@ming-PC>
2016-10-22 23:59:22 -0400

    sv

--- src/Project1.java
+++ src/Project1.java
@@ -4,24 +4,31 @@ public class Project1 {
 
 	public static void main(String[] args)
 	{
-		DoublyLinkedList<Double> a = new DoublyLinkedList<Double>();
-		a.add(3.0);
-		a.add(4.0);
-		a.add(5.0);
-	
-		
-		for (int i =0; i<a.size();i++){
-			System.out.println(a.get(i));
-		}
-		System.out.println("-----------------------");
-//		int k = a.size();
-//		for (int i =0; i<k;i++){
-//			a.remove(a.size()-1);
+//		DoublyLinkedList<Double> a = new DoublyLinkedList<Double>();
+//		a.add(3.0);
+//		a.add(4.0);
+//		a.add(5.0);
+//	
+//		
+//		for (int i =0; i<a.size();i++){
+//			System.out.println(a.get(i));
 //		}
-		a.remove(0);
-		for (int i =0; i<a.size();i++){
-			System.out.println(a.get(i));
-		}
+//		System.out.println("-----------------------");
+////		int k = a.size();
+////		for (int i =0; i<k;i++){
+////			a.remove(a.size()-1);
+////		}
+//		a.remove(0);
+//		for (int i =0; i<a.size();i++){
+//			System.out.println(a.get(i));
+//		}
+//		
+		SparseVector<Integer> sv1 = new SparseVector<>();
+		
+		sv1.add(1,2131);
+		sv1.add(88,232);
+		sv1.add(654,3456);
+		System.out.println(sv1);
 	}
 
 }
--- src/SparseVector.java
+++ src/SparseVector.java
@@ -1,42 +1,55 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
 
-/*
 public class SparseVector<AnyType> 
 {
 
 	private DoublyLinkedList<AnyType> vector = new DoublyLinkedList<AnyType>();
+	
+	private TreeMap<Integer, Integer> indexs = new TreeMap<>();
 	
 	public SparseVector()
 	{
 		this.vector = new DoublyLinkedList<AnyType>();
 	}
 	
+	public void add(int index, AnyType sv)
+	{
+		indexs.put(index, vector.size());
+		vector.add(sv);
+	}
+	
 	public SparseVector add(SparseVector sv)
 	{
 		// returns a SparseVector which is A+B.
-		SparseVector result = new SparseVector();;
+		SparseVector result = new SparseVector();
 		//iterator??
 		return result;
 	}
-	
-	public SparseVector subtract(SparseVector sv)
-	{
-		// returns a SparseVector which is A-B.
-	}
-	
-	 public double dot(SparseVector sv)
-	 {
-		 // public returns a double which is A∙B
-		 
-	 }
+//	
+//	public SparseVector subtract(SparseVector sv)
+//	{
+//		// returns a SparseVector which is A-B.
+//	}
+//	
+//	 public double dot(SparseVector sv)
+//	 {
+//		 // public returns a double which is A∙B
+//		 
+//	 }
+//
+//	
+	public String toString() {
+		String retVal = "(";
+		if (vector != null) {
+		for (Integer ind : indexs.keySet()) {
+			
+				retVal += "[" +ind+", "+vector.get(indexs.get(ind))+"]";
+			}
 
-	
-	public String toString() 
-	{	
-		if(vector != null)
-		{
-			return "[vector=" + vector + "] ";
 		}
+		return retVal+")";
+
 	}
-	 
-	 
-}*/
+
+}
